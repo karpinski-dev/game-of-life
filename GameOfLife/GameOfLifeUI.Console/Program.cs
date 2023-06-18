999e40bfdebcb1a60ae61f30e3eddf5b6ff73dee
Kamil Karpiński <karpinski.dev@gmail.com>
2023-06-18 23:30:30 +0200

    added settings model; changed ui project name

---
+++ GameOfLife/GameOfLifeUI.Console/Program.cs
@@ -0,0 +1,53 @@
+﻿﻿using GameOfLifeProcessor;
+using GameOfLifeProcessor.GameBoard;
+using GameOfLifeUI;
+using Pastel;
+using System.Drawing;
+
+//Settings
+var settings = new SettingsModel();
+
+//Game
+try
+{
+    var game = settings.RandomBoard ? new Game(settings.BoardWidth, settings.BoardHeight) : new Game(settings.BoardWidth, settings.BoardHeight, settings.ActiveCells);
+    for (var i = 0; i < settings.Generations; i++)
+    {
+        Print(game.GetBoard());
+        game.NextGeneration();
+
+        if (settings.DelayOutput)
+        {
+            Thread.Sleep(settings.DelayInMs);
+        }
+
+        if (settings.ClearOutput && i + 1 < settings.Generations)
+        {
+            Console.Clear();
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}".Pastel(Color.FromArgb(247, 52, 52)));
+}
+
+//Output method
+void Print(Board board)
+{
+    for (var y = 0; y < settings.BoardHeight; y++)
+    {
+        for (var x = 0; x < settings.BoardWidth; x++)
+        {
+            var textColor = settings.CellTextColor;
+            if (board.Cells[y, x].IsAlive)
+            {
+                textColor = settings.ActiveCellTextColor;
+            }
+
+            Console.Write(settings.CellCharacter.Pastel(textColor));
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ GameOfLife/GameOfLifeUI.Console/SettingsModel.cs
@@ -0,0 +1,23 @@
+﻿using GameOfLifeProcessor.GameBoard;
+using System.Drawing;
+
+namespace GameOfLifeUI
+{
+    /// <summary>
+    /// Settings
+    /// </summary>
+    internal class SettingsModel
+    {
+        public int Generations { get; set; } = 150;
+        public int BoardWidth { get; set; } = 25;
+        public int BoardHeight { get; set; } = 10;
+        public bool DelayOutput { get; set; } = true;
+        public int DelayInMs { get; set; } = 100;
+        public string CellCharacter { get; set; } = "x";
+        public bool ClearOutput { get; set; } = true;
+        public Color CellTextColor { get; set; } = Color.FromArgb(0, 0, 0);
+        public Color ActiveCellTextColor { get; set; } = Color.FromArgb(66, 245, 72);
+        public bool RandomBoard { get; set; } = true;
+        public IList<ICellCoordinates> ActiveCells { get; set; } = ActiveCellsExamples.Beacon;
+    }
+}
